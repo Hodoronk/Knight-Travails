e50ae4c9a3c3008cf34a4239e335b7598f33402e
Hodoronk <tanasa.andrei@hotmail.com>
2023-10-17 15:54:28 +0300

    moved createBoard() to Tree class, added knightCoords method

--- src/travails.js
+++ src/travails.js
@@ -1,16 +1,58 @@
-const createBoard = () => {
-    const board = []
-    let x = 0;
-    for(let i = 0; i < 8; i++) {
-        for(let x = 0; x< 8; x++) {
 
-            board.push([i, x])
+
+
+class Tree{
+    constructor(array){
+        // this.root = this.buildTree(array)
+    }
+
+    createBoard() {
+        const board = []
+        let x = 0;
+        for(let i = 0; i < 8; i++) {
+            for(let x = 0; x< 8; x++) {
+                board.push([i, x])
+            }
         }
+        return board
+    }
+    knightCoords(coords, array){
+        console.log(array)
+        for(let i = 0; i < array.length; i++) {
+            if(array[i][0] === coords[0] && array[i][1] === coords[1]) {
+                return i;
+            }
+        }
+       return -1
+    }
+    // buildTree(array){
+    //     this.knightCoords()
+    // }
+}
+
+const myTree = new Tree()
+const index = myTree.knightCoords([0,5] , myTree.createBoard())
+console.log(index)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Node{
+    constructor(){
 
     }
-    return board
 }
-const board = createBoard()
-console.log(board)
-
 
